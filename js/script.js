78523545ca023a98a2637bd993eef64fc2856389
David Kim <dkhw90@gmail.com>
2023-02-12 22:37:08 +0100

    Add js to tabbed components

--- js/script.js
+++ js/script.js
@@ -69,3 +69,19 @@ document.querySelector('.nav__links').addEventListener('click', (evt) => {
     section.scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', (evt) => {
+  const clicked = evt.target.closest('.operations__tab');
+  if (!clicked) return;
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  tabsContent.forEach(content => content.classList.remove('operations__content--active'));
+  clicked.classList.add('operations__tab--active');
+
+  // activate content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
